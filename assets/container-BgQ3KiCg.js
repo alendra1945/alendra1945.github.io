1fdd8f0347aa7e8ba301e6ce3c46f1218bab159e
alendra1945 <dwialendraipangselly23@gmail.com>
2024-09-27 11:37:57 +0700

    update ui

--- assets/container-BgQ3KiCg.js
+++
@@ -1,1 +0,0 @@
-import{i as t,a as r,d as s,b as n,t as l}from"./index-CONonrmZ.js";var m=l("<main>");const i=e=>(()=>{var a=m();return t(a,()=>e.children),r(()=>s(a,n("max-w-4xl w-full mx-auto py-20 px-4 md:px-10",e.class))),a})();export{i as C};
--- assets/use-supported-D258_xZj.js
+++
@@ -1,1 +0,0 @@
-import{f as n,aS as o,o as u,k as r}from"./index-CONonrmZ.js";function s(){const[e,t]=n(!1);return o()&&u(()=>{t(!0)}),e}function i(e){const t=s();return r(()=>(t(),!!e()))}export{i as u};
