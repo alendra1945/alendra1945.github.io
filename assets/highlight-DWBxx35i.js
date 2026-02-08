92e87f34af7a84ebb304b2fe926e2eb5c94857a2
alendra1945 <dwialendraipangselly23@gmail.com>
2026-02-08 15:00:50 +0700

    update

---
+++ assets/highlight-DWBxx35i.js
@@ -0,0 +1,1 @@
+import{e as a,j as l,m as n,b as p,i as o,t as c}from"./index-BHTuoOQg.js";var i=c("<span>");const g=s=>{const[e,t]=a(s,["class","children"]);return(()=>{var r=i();return l(r,n({get class(){return p("bg-neutral-100 px-1 py-0.5")}},t),!1,!0),o(r,()=>e.children),r})()};export{g as H};
